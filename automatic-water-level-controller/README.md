28f2af6fe3d57f1087866c57dda4722b2e92ef8b
Amandeep Singh <osdevss@gmail.com>
2018-01-14 22:46:33 +0530

    Updated code to display water level of both tanks

--- automatic-water-level-controller/README.md
+++ automatic-water-level-controller/README.md
@@ -1,5 +1,3 @@
 # AWLC (Automatic water level controller)
 
-This project is my initial home project. I plan to convert my old water level controller with the power of Arduino into new and advance water level controller. It is different for all the controllers that currently exist into market. It will show you the exact water level in percentage(%) from the water tank.
-
-  
+This project is my initial home project. I plan to convert my old water level controller with the power of Arduino into new and advance water level controller. It is different for all the controllers that currently exist in market. It will show you the exact water level in percentage(%) from the water tank.
--- automatic-water-level-controller/automatic-water-level-controller.ino
+++ automatic-water-level-controller/automatic-water-level-controller.ino
@@ -1,6 +1,9 @@
 // ---------------------------------------------------------------------------
 // Program to measure water level with ultrasonic sensor
 // and display it on 7 segment 4 digit display
+// Author: Amandeep Singh <as.smartdevelopers@gmail.com>both
+// Created: 12-04-2016
+// Modified: 14-01-2018 
 // ---------------------------------------------------------------------------
 
 #include <NewPing.h> // Library for Ultra Sonic Sensor
@@ -9,41 +12,99 @@
 /*
  * Config for Ultrasonic sensor
  */
-#define US_PING_PIN  11  // Arduino pin tied to both trigger and echo pins on the ultrasonic sensor.
+#define SONAR_NUM     2 // Number or sensors.
+#define PING_INTERVAL 500 // Milliseconds between pings.
 #define MAX_DISTANCE 300 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
-NewPing sonar(US_PING_PIN, US_PING_PIN, MAX_DISTANCE); // NewPing setup of pin and maximum distance.
+unsigned long pingTimer[SONAR_NUM]; // When each pings.
+unsigned int cm[SONAR_NUM]; // Store ping distances.
+unsigned int per[SONAR_NUM]; // Store distances percentage.
+uint8_t currentSensor = 0; // Which sensor is active.
+ 
+#define US1_TRIG_PIN  7  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define US1_ECHO_PIN  6  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define US2_TRIG_PIN  9  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define US2_ECHO_PIN  8  // Arduino pin tied to echo pin on the ultrasonic sensor.
+
+NewPing sonar[SONAR_NUM] = { // Sensor object array.
+  NewPing(US1_TRIG_PIN, US1_ECHO_PIN, MAX_DISTANCE), // NewPing UC1 setup of pin and maximum distance.
+  NewPing(US2_TRIG_PIN, US2_ECHO_PIN, MAX_DISTANCE) // NewPing UC2 setup of pin and maximum distance.
+};
 
 /*
- * Configuration for 7segment
+ * Configuration for 7segment Display
  */
-const int CLK = 3; //Set the CLK pin connection to the display
-const int DIO = 2; //Set the DIO pin connection to the display
+// Display 1 
+const int CLK1 = 3; //Set the CLK pin connection to the display1
+const int DIO1 = 2; //Set the DIO pin connection to the display1
 
-TM1637Display display(CLK, DIO); //set up the 4-Digit Display. 
+// Display 2
+const int CLK2 = 5; //Set the CLK pin connection to the display2
+const int DIO2 = 4; //Set the DIO pin connection to the display2
+
+TM1637Display display[SONAR_NUM] = {
+  TM1637Display(CLK1, DIO1),
+  TM1637Display(CLK2, DIO2)
+};
+//TM1637Display display(CLK1, DIO1); //set up the 4-Digit Display1.
+//TM1637Display display(CLK2, DIO2); //set up the 4-Digit Display2. 
 
 /*
  * LED to show the moter status
  */
-
-const int LEDPin = 4;
+//const int LEDPin = 10;
  
 /*
  * Config for Water Tank Level
  */
-#define MAX_WATER_LEVEL 1 // Height in cm at which water level is maximum
-#define MIN_WATER_LEVEL 17 // Height in cm at which water level is minimum
 
+typedef struct {
+   int MAX;
+   int MIN;
+} Tank_Water_Level;
 
+Tank_Water_Level TWL[SONAR_NUM] = {
+   { 20, 102 },
+   { 20, 112 }
+};
+ 
+/*// Tank 1 
+#define TANK1_MAX_WATER_LEVEL 20 // Height in cm at which water level is maximum
+#define TANK1_MIN_WATER_LEVEL 102 // Height in cm at which water level is minimum
+
+// Tank 2
+#define TANK2_MAX_WATER_LEVEL 20 // Height in cm at which water level is maximum
+#define TANK2_MIN_WATER_LEVEL 112 // Height in cm at which water level is minimum
+*/
 
 void setup() {
   Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
-  pinMode(LEDPin, OUTPUT); // LED Pin setup
-  display.setBrightness(0x0a); //set the diplay to maximum brightness
+  //pinMode(LEDPin, OUTPUT); // LED Pin setup
+  display[0].setBrightness(0x0a); //set the diplay to maximum brightness
+  display[1].setBrightness(0x0a); //set the diplay to maximum brightness
+
+  pingTimer[0] = millis() + 75; // First ping start in ms.
+
+  for (uint8_t i = 1; i < SONAR_NUM; i++)
+    pingTimer[i] = pingTimer[i - 1] + PING_INTERVAL;
+    
 }
 
 void loop() {
-  int usVal;
+
+  for (uint8_t i = 0; i < SONAR_NUM; i++) {
+    if (millis() >= pingTimer[i]) {
+      pingTimer[i] += PING_INTERVAL * SONAR_NUM;
+      if (i == 0 && currentSensor == SONAR_NUM - 1)
+        oneSensorCycle(); // Do something with results.
+      sonar[currentSensor].timer_stop();
+      currentSensor = i;
+      cm[currentSensor] = 0;
+      sonar[currentSensor].ping_timer(echoCheck);
+    }
+  }
+  
+  /*int usVal;
   int per;
 
   // Get ultrasonic sensor value
@@ -79,18 +140,36 @@ void loop() {
   display.showNumberDec(per); //Display the numCounter value;
 
   delay(1000); // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+
+  */
 }
 
+void echoCheck() { // If ping echo, set distance to array.
+  if (sonar[currentSensor].check_timer())
+    cm[currentSensor] = sonar[currentSensor].ping_result / US_ROUNDTRIP_CM;
+}
 
-/*
- * Function: usPingValueInCm
- * Description: To get the Ultrasonic sensor value using New ping
- * Return: Interger
- */
-int usValueInCm(){
-  int t = sonar.ping(); // Send ping, get ping time in microseconds (uS).
-  t = t / US_ROUNDTRIP_CM;
-  return t;
+void oneSensorCycle() { // Do something with the results.
+  for (uint8_t i = 0; i < SONAR_NUM; i++) {
+    Serial.print(i);
+    Serial.print("=");
+    Serial.print(cm[i]);
+    Serial.print("cm ");
+    Serial.print("\n");
+    
+    // Get percentage of water level
+    per[i] = waterLevelInPercent(cm[i], i);
+
+    // Serial Print
+    Serial.print(per[i]);
+    Serial.print("%");
+    Serial.print("\n");
+
+    display[i].showNumberDec(per[i]); //Display the numCounter value;
+    
+  }
+
+  
 }
 
 /*
@@ -98,27 +177,27 @@ int usValueInCm(){
  * Description: To get the water level in percent
  * Return: Interger
  */
-int waterLevelInPercent(int usVal) {
+int waterLevelInPercent(int usVal, int i) {
   int cusVal = usVal;
 
   // Check if ultrasonic value is pass our condition 
-  if (usVal <= MAX_WATER_LEVEL) {
-    usVal = MAX_WATER_LEVEL;
-  } else if (usVal >= MIN_WATER_LEVEL) {
-    usVal = MIN_WATER_LEVEL;
+  if (usVal <= TWL[i].MAX) {
+    usVal = TWL[i].MAX;
+  } else if (usVal >= TWL[i].MIN) {
+    usVal = TWL[i].MIN;
   } else {
-    usVal = usVal - MAX_WATER_LEVEL;
+    usVal = usVal - TWL[i].MAX;
   }
 
   // Get water tank level length in centimeters
-  int tankLength = MIN_WATER_LEVEL - MAX_WATER_LEVEL;
+  int tankLength = TWL[i].MIN - TWL[i].MAX;
 
   // Get water level value in percentage
   float percentage = 0;
 
-  if(( cusVal  > MAX_WATER_LEVEL)&&( cusVal < MIN_WATER_LEVEL ) ){
+  if(( cusVal  > TWL[i].MAX)&&( cusVal < TWL[i].MIN ) ){
     percentage = (((float(tankLength) - float(usVal))/float(tankLength)) * 100);
-  } else if (cusVal >= MIN_WATER_LEVEL){
+  } else if (cusVal >= TWL[i].MIN){
     percentage = 0;
   } else {
     percentage = 100.00;
